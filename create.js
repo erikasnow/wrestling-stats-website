ffb8bb74873fccad32c62f10dab27afb8f611de3
Erika <essnow@wpi.edu>
2018-09-17 16:42:45 -0400

    added some more databse stuff

---
+++ create.js
@@ -0,0 +1,22 @@
+var sql = require('sqlite3');
+
+// create database
+var db = new sql.Database('tournament.sqlite');
+
+db.serialize(function() {
+
+  // making the table
+  db.run("CREATE TABLE matches (id varchar(100), redName varchar(100), redScore integer, greenName varchar(100), greenScore integer, winner varchar(100), winType varchar(100))");
+
+  // adding data
+  db.run("INSERT INTO matches VALUES ('asdf123', 'E. Snow', 20, 'T. Dure', 4, 'E. Snow', 'Technical Pin')");
+  db.run("INSERT INTO matches VALUES ('jkl321', 'R. Dorman', 4, 'E. Snow', 1, 'R. Dorman', 'Pin')");
+
+  // verify we can query data
+  db.each("SELECT id, redName, redScore, greenName, greenScore, winner, winType FROM movies", function(err, row) {
+      console.log(JSON.stringify(row));
+  });
+
+});
+
+db.close();
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http')
   , fs = require('fs')
   , url = require('url')
+  , sql  = require('sqlite3')
   , port = 8080;
 
 // db setup
