8f3e2ee20b0abc454a08c2d049d19af3b98563e3
Erika <essnow@wpi.edu>
2018-09-20 15:05:42 -0400

    updated the readme before maybe going for a tech achievement

--- crud.js
+++ crud.js
@@ -4,15 +4,34 @@ var matchArr = [];
 function create_row() {
     var x = document.getElementById("createTable");
     x.style.display = "block";
+
+    //disable every button but save and cancel
     var y = document.getElementById("createButton");
     y.setAttribute("disabled", "true");
+    var editBtns = document.getElementsByClassName("editBtn");
+    var delBtns = document.getElementsByClassName("delBtn");
+    for (let i = 0; i < editBtns.length; i++) {
+        //could separate these, but there should be same number of each
+        editBtns[i].setAttribute("disabled", "true");
+        delBtns[i].setAttribute("disabled", "true");
+    }
 }
 function save_row() {
     var x = document.getElementById("createTable");
     x.style.display = "none";
+
+    //enable all the disabled buttons
     var y = document.getElementById("createButton");
     y.removeAttribute("disabled");
+    var editBtns = document.getElementsByClassName("editBtn");
+    var delBtns = document.getElementsByClassName("delBtn");
+    for (let i = 0; i < editBtns.length; i++) {
+        //could separate these, but there should be same number of each
+        editBtns[i].removeAttribute("disabled");
+        delBtns[i].removeAttribute("disabled");
+    }
 
+    //grab data to send to server
     var match = {};
     match.redName = document.getElementById('cRedName').innerHTML;
     match.redScore = document.getElementById('cRedScore').innerHTML;
@@ -25,9 +44,9 @@ function save_row() {
     var req = new XMLHttpRequest();
     req.open('POST', '/create');
     req.responseType = 'text';
-    req.onload = function() { 
-        console.log('create done!'); 
-        location.reload(); 
+    req.onload = function () {
+        console.log('create done!');
+        location.reload();
     }
     req.send(JSON.stringify(match));
 }
@@ -36,14 +55,34 @@ function cancel() {
     x.style.display = "none";
     var y = document.getElementById("updateTable");
     y.style.display = "none";
+
+    //enable all the disabled buttons
     var z = document.getElementById("createButton");
     z.removeAttribute("disabled");
+    var editBtns = document.getElementsByClassName("editBtn");
+    var delBtns = document.getElementsByClassName("delBtn");
+    for (let i = 0; i < editBtns.length; i++) {
+        //could separate these, but there should be same number of each
+        editBtns[i].removeAttribute("disabled");
+        delBtns[i].removeAttribute("disabled");
+    }
 }
 function edit_row(id) {
     var x = document.getElementById("updateTable");
     x.style.display = "block";
 
-    match_current = matchArr.filter( function(d){ return d.id === id; } )[0];
+    //disable every button but update and cancel
+    var y = document.getElementById("createButton");
+    y.setAttribute("disabled", "true");
+    var editBtns = document.getElementsByClassName("editBtn");
+    var delBtns = document.getElementsByClassName("delBtn");
+    for (let i = 0; i < editBtns.length; i++) {
+        //could separate these, but there should be same number of each
+        editBtns[i].setAttribute("disabled", "true");
+        delBtns[i].setAttribute("disabled", "true");
+    }
+
+    match_current = matchArr.filter(function (d) { return d.id === id; })[0];
     console.log("match_current: " + JSON.stringify(match_current));
     document.getElementById('uId').innerHTML = match_current.id;
     document.getElementById('uRedName').innerHTML = match_current.redName;
@@ -56,6 +95,17 @@ function edit_row(id) {
 function update_row() {
     var x = document.getElementById("updateTable");
     x.style.display = "none";
+
+    //enable all the disabled buttons
+    var z = document.getElementById("createButton");
+    z.removeAttribute("disabled");
+    var editBtns = document.getElementsByClassName("editBtn");
+    var delBtns = document.getElementsByClassName("delBtn");
+    for (let i = 0; i < editBtns.length; i++) {
+        //could separate these, but there should be same number of each
+        editBtns[i].removeAttribute("disabled");
+        delBtns[i].removeAttribute("disabled");
+    }
 
     var match = {};
     match.id = document.getElementById('uId').innerHTML;
@@ -70,54 +120,54 @@ function update_row() {
     var req = new XMLHttpRequest();
     req.open('PUT', '/update');
     req.responseType = 'text';
-    req.onload = function() { 
-        console.log('done!'); 
-        location.reload(); 
+    req.onload = function () {
+        console.log('done!');
+        location.reload();
     }
     req.send(JSON.stringify(match));
 }
-function delete_row(id){
+function delete_row(id) {
     console.log("entered delete function");
-    match_current = matchArr.filter( function(d){ return d.id === id; } )[0];
+    match_current = matchArr.filter(function (d) { return d.id === id; })[0];
     console.log("match_current: " + JSON.stringify(match_current));
 
     var req = new XMLHttpRequest();
     req.open('POST', '/delete');
     req.responseType = 'text';
-    req.onload = function() { 
-        console.log('delete done!'); 
-        location.reload(); 
+    req.onload = function () {
+        console.log('delete done!');
+        location.reload();
     }
     req.send(JSON.stringify(match_current));
 }
 function build(rows) {
-   for(let i = 0; i < rows.length; i++){
-       console.log("entered build loop");
-       var row = document.getElementById("results").insertRow(i+2);
-       var rn = row.insertCell(0);
-       var rs = row.insertCell(1);
-       var gn = row.insertCell(2);
-       var gs = row.insertCell(3);
-       var w = row.insertCell(4);
-       var wt = row.insertCell(5);
+    for (let i = 0; i < rows.length; i++) {
+        console.log("entered build loop");
+        var row = document.getElementById("results").insertRow(i + 2);
+        var rn = row.insertCell(0);
+        var rs = row.insertCell(1);
+        var gn = row.insertCell(2);
+        var gs = row.insertCell(3);
+        var w = row.insertCell(4);
+        var wt = row.insertCell(5);
 
-       rn.innerHTML = rows[i].redName;
-       rs.innerHTML = rows[i].redScore;
-       gn.innerHTML = rows[i].greenName;
-       gs.innerHTML = rows[i].greenScore;
-       w.innerHTML = rows[i].winner;
-       wt.innerHTML = rows[i].winType;
+        rn.innerHTML = rows[i].redName;
+        rs.innerHTML = rows[i].redScore;
+        gn.innerHTML = rows[i].greenName;
+        gs.innerHTML = rows[i].greenScore;
+        w.innerHTML = rows[i].winner;
+        wt.innerHTML = rows[i].winType;
 
-       var editBtn = row.insertCell(6);
-       editBtn.style.width = "1%";
-       var delBtn = row.insertCell(7);
-       delBtn.style.width = "1%";
+        var editBtn = row.insertCell(6);
+        editBtn.style.width = "1%";
+        var delBtn = row.insertCell(7);
+        delBtn.style.width = "1%";
 
-       var iden = rows[i].id;
+        var iden = rows[i].id;
 
-       editBtn.innerHTML = '<button onclick="edit_row(\'' + iden + '\')">Edit</button>';
-       delBtn.innerHTML = '<button onclick="delete_row(\'' + iden + '\')">Delete</button>';
-   }
+        editBtn.innerHTML = '<button class="editBtn" onclick="edit_row(\'' + iden + '\')">Edit</button>';
+        delBtn.innerHTML = '<button class="delBtn" onclick="delete_row(\'' + iden + '\')">Delete</button>';
+    }
 }
 function read() {
     console.log("Sending read request");
